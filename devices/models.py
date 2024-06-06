9605d74f192c4786fc7ed53c294d17e71db1f69b
joão laurindo <joaoviniciuslaurindo@live.com>
2024-06-06 20:00:01 -0300

    Commit desesperado pra ficar tudo pronto em tempo e não perder mais nada socorro

--- devices/models.py
+++ devices/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Stac(models.Model):
+    pass
+
+class CollectedData(models.Model):
+    pass
+
+class StacVersion():
+    pass
--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Plant (models.Model):
+    pass
+    
+class PlanttingType(models.Model):
+    pass
+
+class GrowthStages(models.Model):
+    pass
+class Planting (models.Model):
+    pass
+
